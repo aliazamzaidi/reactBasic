35241a3c328bf8e41bca2c951474cfdb7a4833c0
Ali Azam <Ali Azam>
2017-04-04 13:15:30 +0500

    tutorial 3

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-12'>
+            <p>Hello! World</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
